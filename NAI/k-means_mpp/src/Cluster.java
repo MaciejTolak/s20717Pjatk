5cf5cb56ecf033a5c6683000941bd7db39407756
Maciej Tolak <82982235+MaciejTolak@users.noreply.github.com>
2021-05-26 18:32:13 +0200

    Add files via upload

---
+++ NAI/k-means_mpp/src/Cluster.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cluster {
+    List<Double> wartosci;
+    List<Vector> list;
+
+    public Cluster(List<Double> wartosci) {
+        this.wartosci = wartosci;
+        list = new ArrayList<>();
+    }
+
+    public boolean check(){
+        int counter = 0;
+        double tmp;
+        for(int i =0; i<wartosci.size(); i++){
+            tmp =0;
+            for(Vector vector : list){
+                tmp += vector.wartosci.get(i);
+            }
+            tmp = tmp/list.size();
+            if(tmp == wartosci.get(i)){
+                counter++;
+            }
+        }
+        if(counter == wartosci.size()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void changeCentroid(){
+        double tmp;
+        for(int i =0; i<wartosci.size(); i++) {
+            tmp = 0;
+            for (Vector vector : list) {
+                tmp += vector.wartosci.get(i);
+            }
+            tmp = tmp/list.size();
+            this.wartosci.set(i,tmp);
+        }
+
+    }
+
+    public void clearList(){
+        list.clear();
+    }
+
+}
